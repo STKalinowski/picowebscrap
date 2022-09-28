07917e2fec986ebf5496cb987faf1f70d7d6f8d6
STKalinowski <kalinowski.stas@gmail.com>
2022-09-27 21:11:32 -0700

    Done, looks like its working, no tests.

--- scrap.py
+++ scrap.py
@@ -1,32 +1,28 @@
 import requests
-import os
 import csv
-from io import StringIO
+import os
 from bs4 import BeautifulSoup
 
-from lxml import etree
-import lxml
-
-
-
 INPUT_CSV = "./pico8-carts_5_24_22_FeaturedCarts.csv"
+#INPUT_CSV = "./test.csv"
 CART_FOLDER = "./result/Carts/"
 RES_CSV = "./result/dataset.csv"
                 
 def main():
     with open(INPUT_CSV) as csvFile:
         print("Opening "+ RES_CSV)
-        with open(RES_CSV) as resultFile:
+        with open(RES_CSV, 'w', newline='') as resultFile:
             csvReader = csv.reader(csvFile, delimiter=',')
+            csvWriter = csv.writer(resultFile, delimiter=',')
             headers = next(csvReader)
             headers.append("License,Description,FileName")
-            csvWriter = csv.writer(resultFile, delimiter=',')
-
+            csvWriter.writerow(headers)
+            
             for entry in csvReader:
                 link = entry[4]
                 res = requests.get(link)
                 soup = BeautifulSoup(res.content, 'html.parser')
-                print(link)
+                assert(res.status_code == 200)
 
                 #Handle Liscence
                 soupRes = soup.find_all(string="License:")    
@@ -41,18 +37,25 @@ def main():
                 description = ""
                 soupRes = soup.select("#main_div > div:nth-child(5) > div:nth-child(1) > div > div > div:nth-child(2) > div:nth-child(3) > p")#.find_all("p")
                 for i in soupRes[1:]:
-                    description += i.text + "\n"
+                    description += i.text + " "
                 #Get rid of last newline
                 description = description[:-1]
 
                 #Handle Cart
                 #Turn into p8, then get code and make lua copy
-                fileName = entry[3].strip()+".p8.png"
+                fileName = ''.join(filter(str.isalpha, entry[3]))
                 soupRes = soup.find_all(string="Cart")
                 pngFile = requests.get("https://www.lexaloffle.com"+soupRes[0].parent['href']) 
-                with open("./png/"+fileName, 'wb') as f:
+                with open("./result/png/"+fileName+".p8.png", 'wb') as f:
                     f.write(pngFile.content)
-                break
+                #Now convert .p8.png to .p8
+
+                #Now extract code from .p8 to .lua file
+
+                #Write new csv entry
+                csvWriter.writerow(entry+[license, description, fileName])
+                os.system("python3 ./shrinko8/shrinko8.py ./result/png/"+fileName+".p8.png ./result/p8/"+fileName+".p8")
+                os.system("python3 ./shrinko8/shrinko8.py ./result/png/"+fileName+".p8.png ./result/lua/"+fileName+".lua")
 
 
 main()     
